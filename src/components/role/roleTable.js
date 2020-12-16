69d3fb1cbd59d14b06c68c418f4ecb3340e3a1b1
ikechuku <sir_ik@yahoo.com>
2020-12-16 10:53:59 +0100

    fix "undefined" bug in request

--- src/components/role/roleTable.js
+++ src/components/role/roleTable.js
@@ -2,69 +2,72 @@ import React, { Component } from "react";
 import Table from "../../helpers/customTable";
 
 class RoleTable extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	getValue =  (data) => {
-		const firstDept = data.unit !== null ? data.unit.department.name : '' ;
-		const unitName = data.unitId !== null ? data.unit.name : '';
+  getValue = (data) => {
+    const firstDept = data.unit !== null ? data.unit.department.name : "";
+    const unitName =
+      data.unitId !== null && data.unit != null ? data.unit.name : "";
 
-		return data.unitId === null ? data.department.name : (firstDept + '/' + unitName);
-	}
+    return data.unitId === null
+      ? data.department.name
+      : firstDept + "/" + unitName;
+  };
 
-	bodyRow = () => {
-		const body = this.props.roles.map((data, index) => ({
-			jobTitle: data.title,
-			departmentUnit: this.getValue(data),
-			action: (
-				<a>
-					<span
-						className="edit"
-						data-toggle="modal"
-						data-target="#roleModal"
-						onClick={() => this.props.handleEdit(data.id)}
-					>
-						Edit
-					</span>
-					{/* <span className="del" 
+  bodyRow = () => {
+    const body = this.props.roles.map((data, index) => ({
+      jobTitle: data.title,
+      departmentUnit: this.getValue(data),
+      action: (
+        <a>
+          <span
+            className="edit"
+            data-toggle="modal"
+            data-target="#roleModal"
+            onClick={() => this.props.handleEdit(data.id)}
+          >
+            Edit
+          </span>
+          {/* <span className="del" 
 						data-toggle="modal"
 						data-target="#confirm"
 						onClick={() => this.props.setSelectedId(data.id)}>
 						Delete
 					</span> */}
-				</a>
-			),
-		}));
-		return body;
-	};
+        </a>
+      ),
+    }));
+    return body;
+  };
 
-	header = () => {
-		const header = [
-			{
-				title: "Job Title (filterable)",
-				prop: "jobTitle",
-				sortable: true,
-				filterable: true,
-			},
-			{ title: "Department/Unit", prop: "departmentUnit", sortable: true },
-			{ title: "Actions", prop: "action" },
-		];
-		return header;
-	};
+  header = () => {
+    const header = [
+      {
+        title: "Job Title (filterable)",
+        prop: "jobTitle",
+        sortable: true,
+        filterable: true,
+      },
+      { title: "Department/Unit", prop: "departmentUnit", sortable: true },
+      { title: "Actions", prop: "action" },
+    ];
+    return header;
+  };
 
-	render() {
-		return (
-			<div>
-				<div className="table-responsive" style={{ overflow: "hidden" }}>
-					<Table
-						body={this.bodyRow}
-						head={this.header}
-						rowsPerPage={10}
-						rowsPerPageOption={[10, 15, 20, 25]}
-					/>
+  render() {
+    return (
+      <div>
+        <div className="table-responsive" style={{ overflow: "hidden" }}>
+          <Table
+            body={this.bodyRow}
+            head={this.header}
+            rowsPerPage={10}
+            rowsPerPageOption={[10, 15, 20, 25]}
+          />
 
-					{/* <table className="table table-bordered table-hover mb-0 text-nowrap">
+          {/* <table className="table table-bordered table-hover mb-0 text-nowrap">
                     <tr>
                         <th>Job Title</th>
                         <th>Department/Unit</th>
@@ -91,10 +94,10 @@ class RoleTable extends Component {
 
 												
 												</table> */}
-				</div>
-			</div>
-		);
-	}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RoleTable;
