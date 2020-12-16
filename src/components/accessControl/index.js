97f6efb48881b433b48ac7fad0a9a092c6ecc7fa
ikechuku <sir_ik@yahoo.com>
2020-12-16 10:49:39 +0100

    create accessControl component to enforce privileges

---
+++ src/components/accessControl/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AccessControl = ({
+  userPermissions,
+  allowedPermissions,
+  children,
+  renderNoAccess,
+}) => {
+  // checkPermissions function verifies that one of the userPermissions is in the set of allowedPermissions
+  const permitted = checkPermissions(userPermissions, allowedPermissions);
+
+  if (permitted) {
+    return children;
+  }
+  return renderNoAccess();
+};
+
+export default AccessControl;
